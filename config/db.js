77d4052841ea53eee49e563f88db5fded4bfc883
ashuchauhan5120-hub <ashuchauhan5120@gmail.com>
2026-02-22 11:37:29 +0530

    db issue

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL); 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
